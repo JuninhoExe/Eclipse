33132963adda34b7c10e029728436ee19b89b030
Ezequiel Junior <104368933+JuninhoExe@users.noreply.github.com>
2022-09-16 11:37:02 -0300

    Add files via upload

---
+++ poo_exercicio/src/boletim/Aluno.java
@@ -0,0 +1,15 @@
+package boletim;
+
+public class Aluno {
+	String nome;
+	String disciplina;
+	float n1,n2,n3,n4;
+    float media;
+	
+public void disciplina() {
+	disciplina = "Programação de Aplicativos";
+}
+void nota() {
+
+}
+}
